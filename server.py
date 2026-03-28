d1fdd3e47a81488ba1939161dc3f7e9045caae43
ivan0796223-byte <ivan0796223@gmail.com>
2026-03-28 10:28:15 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+ hfrom flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -72,4 +72,4 @@ def chat_get():
 
 # ===== ЗАПУСК =====
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000)ttp://127.0.0.1:5000
