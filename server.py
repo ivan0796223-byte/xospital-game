ba80ed17cc0aa59d53c9106892c7dca81bb99200
ivan0796223-byte <ivan0796223@gmail.com>
2026-03-29 02:29:57 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,34 +1,28 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.secret_key = "secret"
 
-# --- Главная ---
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# --- Пациенты ---
 @app.route("/patients")
 def patients():
     return render_template("patients.html")
 
-# --- Лаборатория ---
 @app.route("/lab")
 def lab():
     return render_template("lab.html")
 
-# --- Операционная ---
 @app.route("/operating")
 def operating():
     return render_template("operating.html")
 
-# --- Кабинет доктора ---
 @app.route("/doctor")
 def doctor():
     return render_template("doctor.html")
 
-# --- Обменник ---
 @app.route("/exchange")
 def exchange():
     return render_template("exchange.html")
