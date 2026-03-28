93268e8d1c6655e0f68ec891dc6f8e54f1602171
ivan0796223-byte <ivan0796223@gmail.com>
2026-03-29 06:28:34 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("patients.html")
+# Игрок
+player = {
+    "money": 1000,
+    "exp": 0
+}
 
+# Главная
+@app.route("/")
+def index():
+    return render_template("index.html", player=player)
+
+# Пациенты
+@app.route("/patients")
+def patients():
+    return render_template("patients.html", player=player)
+
+# Действия
 @app.route("/action", methods=["POST"])
 def action():
     action = request.form.get("action")
 
-    if action == "patient1":
-        return "Лечение пациента 1"
-    elif action == "patient2":
-        return "Лечение пациента 2"
-    elif action == "patient3":
-        return "Лечение пациента 3"
+    if action == "heal1":
+        player["money"] += 100
+        player["exp"] += 10
 
-    return "Ошибка"
+    elif action == "heal2":
+        player["money"] += 200
+        player["exp"] += 20
+
+    elif action == "heal3":
+        player["money"] += 300
+        player["exp"] += 30
+
+    return redirect(url_for("patients"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
