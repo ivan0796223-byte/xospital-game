7d70348b864f1b232fbe574610732366f78279c0
ivan0796223-byte <ivan0796223@gmail.com>
2026-03-30 06:55:34 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,17 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# ================== ГЛАВНАЯ ==================
 @app.route("/")
 def home():
-    return "🏥 Hospital Game работает!"
+    return "OK - Hospital Game работает"
 
-# ================== РЕКВИЗИТЫ ==================
 @app.route("/rekvizity")
 def rekvizity():
     return render_template("rekvizity.html")
 
 
-# ================== ЗАПУСК ==================
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
