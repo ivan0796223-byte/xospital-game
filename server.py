548795d9b4f3ed3270c44331ab27ddfc78e7cdce
ivan0796223-byte <ivan0796223@gmail.com>
2026-03-31 15:57:50 +0800

    Update server.py

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-# Настроим логирование
+# Настраиваем логирование
 logging.basicConfig(level=logging.INFO)
 
 # ===== МОДЕЛЬ =====
