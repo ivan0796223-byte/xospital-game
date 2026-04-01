9dc30c34f94135accecd4ff62df7b730e23d6428
ivan0796223-byte <ivan0796223@gmail.com>
2026-04-01 10:04:17 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,35 +2,16 @@ from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 app.secret_key = "123"
-
 users = {}
-
-@app.route("/")
-def home():
-    return redirect("/register")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         login = request.form.get("login")
         password = request.form.get("password")
-
         users[login] = password
         return redirect("/login")
-
     return render_template("register.html")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        session["user"] = request.form.get("login")
-        return redirect("/dashboard")
-
-    return render_template("login.html")
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
