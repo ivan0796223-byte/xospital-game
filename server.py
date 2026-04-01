bd2d393a96fcfa8bb0e60c4da7495c7abb19691d
ivan0796223-byte <ivan0796223@gmail.com>
2026-04-01 09:09:02 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, session
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 app.secret_key = "secret123"
@@ -7,7 +7,9 @@ users = {}
 
 @app.route("/")
 def home():
-    return "Главная работает"
+    if "user" in session:
+        return redirect("/dashboard")
+    return redirect("/login")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -16,28 +18,21 @@ def register():
         password = request.form.get("password")
 
         users[login] = {"password": password}
-        return "ЗАРЕГАНО"
+        return redirect("/login")
 
-    return '''
-    <form method="post">
-        <input name="login">
-        <input name="password">
-        <button type="submit">OK</button>
-    </form>
-    '''
+    return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        return "ЛОГИН ОК"
+        session["user"] = request.form.get("login")
+        return redirect("/dashboard")
 
-    return '''
-    <form method="post">
-        <input name="login">
-        <input name="password">
-        <button type="submit">OK</button>
-    </form>
-    '''
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
