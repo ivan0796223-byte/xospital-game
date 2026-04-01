b921b88eee0531e9349131f1a2bbf82b01f489f3
ivan0796223-byte <ivan0796223@gmail.com>
2026-04-01 09:43:09 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "secret123"
+
+users = {}
 
 @app.route("/")
 def home():
-    return render_template("test.html")
+    return "SERVER WORKS"
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        login = request.form.get("login")
+        password = request.form.get("password")
+
+        users[login] = password
+        return "REGISTER OK"
+
+    return '''
+    <h1>Регистрация</h1>
+    <form method="post">
+        <input name="login">
+        <input name="password">
+        <button type="submit">OK</button>
+    </form>
+    '''
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
